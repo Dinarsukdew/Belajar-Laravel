f1ce63290748e03b44014e394af00f952c1f8440
Dinar <dnrskm@gmail.com>
2019-10-04 21:35:57 +0700

    Percobaan sampe blade

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA_Compatible" content="ie=edge">
+
+        <title>About</title>
+
+        <style>
+            body {
+                background-color: #ff0;
+            }
+        </style>
+        
+    </head>
+    <body>
+
+    	@include('layout.partials.navigation')
+
+        @yield('content')
+    	
+    </body>
+</html>
---
+++ resources/views/layout/partials/navigation.blade.php
@@ -0,0 +1,6 @@
+<ul>
+	<li><a href="/">Welcome</li>
+    <li><a href="/about">About</li>
+    <li><a href="/contact">Contact</li>
+    <li><a href="/article">Article</li>
+</ul>
